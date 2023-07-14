6088cb6af2c35daa2ddac4f288033f59a70dedae
shashidherreddy <shashidherreddy68.com>
2023-07-14 02:48:18 -0500

    added custom tags

--- Blogapp/blog/templatetags/blogtags.py
+++ Blogapp/blog/templatetags/blogtags.py
@@ -1,6 +1,7 @@
 from django import template
 from ..models import Post
-from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
 
 register = template.Library()
 
@@ -13,3 +14,12 @@ def total_posts():
 @register.simple_tag(name="published_posts")
 def published_posts():
     return Post.published.count()
+
+@register.inclusion_tag('blog/post/latest_post.html')
+def show_latest_post(count = 5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return{"latest_post": latest_posts}
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
